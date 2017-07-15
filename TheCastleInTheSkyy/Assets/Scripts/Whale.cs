f6efc30d4a80412147e644e4e3b5ac77bdd882da
DeviantSchemist <michaelngo1337@yahoo.com>
2017-07-15 15:27:22 -0700

    Added whale class

---
+++ TheCastleInTheSkyy/Assets/Scripts/Whale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onCollisionEnter(Collision2D collision)
+    {
+
+    }
+}
