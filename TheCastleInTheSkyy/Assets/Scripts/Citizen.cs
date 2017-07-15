a56da9cc6f598310c01f084e0c8a581caeb9a935
DeviantSchemist <michaelngo1337@yahoo.com>
2017-07-15 07:31:03 -0700

    Made citizen and soldier scripts

--- TheCastleInTheSkyy/Assets/Scripts/Citizen.cs
+++ TheCastleInTheSkyy/Assets/Scripts/Citizen.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Citizen : MonoBehaviour {
+
+    private static float ageInt;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        ageInt = 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void spawn()
+    {
+        //spawn mechanic
+    }
+
+    public void aging()
+    {
+        //how fast each class ages
+    }
 }
