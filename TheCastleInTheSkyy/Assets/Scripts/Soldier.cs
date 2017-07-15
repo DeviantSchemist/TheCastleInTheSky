a80e6465be376cc6fb33de1479aff1e8cf11aa7a
DeviantSchemist <michaelngo1337@yahoo.com>
2017-07-15 13:07:58 -0700

    Made weapon and soldier script

--- TheCastleInTheSkyy/Assets/Scripts/Soldier.cs
+++ TheCastleInTheSkyy/Assets/Scripts/Soldier.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Soldier : Civilian {
 
+    private Weapon myWeapon;
+
 	// Use this for initialization
 	void Start () {
-		
+        myWeapon = new Weapon();
 	}
 	
 	// Update is called once per frame
