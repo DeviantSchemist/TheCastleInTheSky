ea2919d136e2e8390f340a9115ae577eb6af0cd4
Nhel10 <lnyonzon@gmail.com>
2017-07-15 13:30:06 -0700

    Created basic script with castle behavior
    Work in progress

---
+++ TheCastleInTheSkyy/Assets/CastleBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CastleBehavior : MonoBehaviour {
+
+	//  Castle Class 
+
+	public class Castle{
+		public int citizenCount;		// Amount of citizens in the castle
+		public int maxCapacity;			// Maximum capacity of citizens to keep castle floating 
+		public int heightDistance;		// Distance of the castle from the ground 
+		public int dayCounter;			// Keeps track of the amount of days that pass by 
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
