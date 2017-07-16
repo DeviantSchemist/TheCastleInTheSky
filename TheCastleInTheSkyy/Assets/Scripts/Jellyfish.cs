0dab51041f9066ccb628a59355d3ff7652f8e787
DeviantSchemist <michaelngo1337@yahoo.com>
2017-07-16 14:57:02 -0700

    Altered size of sprites and jellyfish movement

--- TheCastleInTheSkyy/Assets/Scripts/Jellyfish.cs
+++ TheCastleInTheSkyy/Assets/Scripts/Jellyfish.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Jellyfish : MonoBehaviour {
 
     private const float JELLY_WEIGHT = 30f;
+    public Transform target;
+    public float speed;
 
 	// Use this for initialization
 	void Start () {
-		
+        target = GameObject.Find("Ship").GetComponent<Transform>();
+        speed = 5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float step = speed * Time.deltaTime;  // movement speed
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);  // gradual movement
 	}
 
     void onCollisionEnter(Collision2D collision)
