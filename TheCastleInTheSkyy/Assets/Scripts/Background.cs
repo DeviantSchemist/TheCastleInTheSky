174ada59b728db3e79bb4495ccbdd9823538289e
DeviantSchemist <michaelngo1337@yahoo.com>
2017-07-16 17:00:55 -0700

    more background adjustments

--- TheCastleInTheSkyy/Assets/Scripts/Background.cs
+++ TheCastleInTheSkyy/Assets/Scripts/Background.cs
@@ -7,7 +7,7 @@ public class Background : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         SpriteRenderer sprite = GameObject.Find("Layer4Background").GetComponent<SpriteRenderer>();
-        Camera.main.orthographicSize = sprite.bounds.size.x * Screen.height / Screen.width * 0.5f;
+        Camera.main.orthographicSize = sprite.bounds.size.x * Screen.height / Screen.width * 1f;
     }
 	
 	// Update is called once per frame
