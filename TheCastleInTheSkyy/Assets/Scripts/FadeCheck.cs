3ee35c624634fd55fbfef528c8f7173f550dc65a
DeviantSchemist <michaelngo1337@yahoo.com>
2017-07-16 22:39:59 -0700

    deleted stuff

--- TheCastleInTheSkyy/Assets/Scripts/FadeCheck.cs
+++ TheCastleInTheSkyy/Assets/Scripts/FadeCheck.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FadeCheck : MonoBehaviour {
 
+    
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +15,11 @@ public class FadeCheck : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.gameObject.name == "Boundary")
+        {
+            
+        }
+    }
 }
