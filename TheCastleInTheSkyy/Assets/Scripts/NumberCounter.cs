1d7e163688580bb1e22840d5248f189dd638fb19
Nhel10 <lnyonzon@gmail.com>
2017-07-16 20:36:22 -0700

    UI Placement

---
+++ TheCastleInTheSkyy/Assets/Scripts/NumberCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberCounter : MonoBehaviour {
+
+
+	public Sprite number3;
+	public Sprite number4;
+	public Sprite number5;
+	public Sprite number6;
+	public Sprite number7;
+	public Sprite number8;
+	public Sprite number9;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
